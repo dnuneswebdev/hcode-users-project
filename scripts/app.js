47980216659aafd8cf6606f7aca6ad890b199ae3
Daniel <dnuneswebdev@gmail.com>
2018-06-18 21:39:21 -0300

    Builded a listener to a form submit, created a JSON and called a function to create a new user html table

--- scripts/app.js
+++ scripts/app.js
@@ -12,15 +12,41 @@
 //INITIALIZE JSON
 let user = {};
 
-//SELECTORS USING FOREACH WITH "NAME" ATTRIBUTE
+//FORM SELECTOR WITH "NAME" ATTRIBUTE
 let fields = document.querySelectorAll('#form-user-create [name]');
 
-fields.forEach((field, index) => {
-  if(field.name == 'gender' && field.checked) {
-    user[field.name] = field.value //cria o JSON com o gender
-  } else {
-    user[field.name] = field.value; // cria o restante do JSON
-  }
+//GET THE FORM, CREATE THE JSON FILE AND CALL ADDUSER FUNCTION
+document.getElementById('form-user-create').addEventListener('submit', e => {
+  e.preventDefault();
+
+  //LOOP THROUGH ALL THE SELECTORS IN THE FORM WITH "NAME" ATTRIBUTE AND FILTER GENDER
+  fields.forEach((field, index) => {
+    if(field.name == 'gender' && field.checked) {
+      user[field.name] = field.value //CREATE THE JSON WITH THE GENDER
+    } else {
+      user[field.name] = field.value; //CREATE THE REST OF THE JSON
+    }
+  });
+    addUser(user);
 });
 
-console.log(user)
+//CREATE THE USER INSIDE THE TABLE DIV
+function addUser(user) {
+  let tr = document.createElement('tr');
+
+  tr.innerHTML = `
+    <tr>
+    <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+    <td>${user.name}</td>
+    <td>${user.email}</td>
+    <td>${user.admin}</td>
+    <td>${user.birth}</td>
+    <td>
+      <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+      <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+    </td>
+  </tr>
+  `
+  document.getElementById('table-users').appendChild(tr);
+  console.log(user)
+}
